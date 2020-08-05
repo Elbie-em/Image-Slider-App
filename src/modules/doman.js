d87ea39e3241fb1355f7c095503dcd4d46d654ab
Elbie-em <emoonga1993@gmail.com>
2020-08-05 22:12:08 +0200

    Import styles.css, Add activate method

---
+++ src/modules/doman.js
@@ -0,0 +1,9 @@
+import '../css/styles.css';
+
+const activate = () => {
+  const slider = document.getElementById('sliderContainer');
+  slider.className = 'slider mx-auto';
+
+}
+
+export {activate}
