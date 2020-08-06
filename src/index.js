266304dc33d97a3c05e514a8ce497057854e1b7e
Elbie-em <emoonga1993@gmail.com>
2020-08-06 15:58:42 +0200

    Modify transition effects

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ let imgs = ["https://cdn.hipwallpaper.com/i/95/94/d2Jnk5.jpg",
 let slide = Doman.getItem('slideObject');
 let index = 0;
 
+let millisec = 2000;
 let img = Doman.createItem('img');
 img.className = 'w-100 h-100'
 img.src = imgs[0];
@@ -21,37 +22,62 @@ Doman.addItem(slide,img);
 
 const previous = () => {
  slide.innerHTML = '';
- index--;
+ index -= 1;
  if (index < 0 ){
-   index = 0
+   index = 0;
    img.className = 'w-100 h-100';
  }else if(index >= 0){
-  img.className = 'w-100 h-100 move-back'
+  img.className = 'w-100 h-100 move-back';
   setTimeout(function(){ img.src = imgs[index]; }, 500);
  }
  Doman.addItem(slide,img);
 }
 
 const next = () => {
-  index++;
+  index += 1;
   if(index > imgs.length - 1 ){
-    index = imgs.length - 1
+    index = imgs.length - 1;
     img.className = 'w-100 h-100';
-  }else if(index <= imgs.length - 1){
+  }else if(index < imgs.length ){
     img.className = 'w-100 h-100 move-front'
     setTimeout(function(){ img.src = imgs[index]; }, 500);
   }
   Doman.addItem(slide,img);
 }
 
+let reached = false;
+
+const auto = () => setInterval(function(){ 
+  if (reached === false){
+    next(); 
+    if(index === imgs.length - 1 ){
+      index += 1;
+      reached = true;
+    }
+  }
+  if(reached === true){
+    previous();
+    if(index === 0){
+      reached = false;
+    }
+  }
+  console.log(index);
+  console.log(reached);
+}, millisec);
+
+//auto();
+
 const prevBtn  = Doman.getItem('prevBtn');
 prevBtn.onclick = () => {
  previous();
+ clearInterval(auto);
 }
 
 const nextBtn = Doman.getItem('nextBtn');
 nextBtn.onclick = () => {
   next();
+  clearInterval(auto);
 }
 
-setInterval(function(){ next(); }, 5000);
+
+
