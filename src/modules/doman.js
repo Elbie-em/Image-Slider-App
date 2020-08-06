0994eb4e0ecfd393637c572bfe1e0bdceeaafc6c
Elbie-em <emoonga1993@gmail.com>
2020-08-06 13:54:27 +0200

    Add getitem, createitem and additem methods

--- src/modules/doman.js
+++ src/modules/doman.js
@@ -3,9 +3,24 @@ import '../css/styles.css';
 const activate = () => {
   const slider = document.getElementById('sliderContainer');
   slider.className = 'slider mx-auto';
-
   document.body.className = 'bg-back';
 
 }
 
-export {activate}
+const getItem = (id) => {
+  const elem = document.getElementById(id);
+  return elem;
+}
+
+const createItem = (name) => {
+  const elem = document.createElement(name);
+  return elem;
+}
+
+const addItem = (container,elem) => {
+  container.appendChild(elem)
+}
+
+
+
+export {activate,getItem,createItem,addItem}
